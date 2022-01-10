151801cb4eafb67e2fee8d2089d89e6ad048bb26
Oliver R <whirlz2300@hotmail.com>
2022-01-10 20:29:20 +0100

    Updated front end to better reflect status. Styling update package update

--- server.js
+++ server.js
@@ -1,15 +1,31 @@
 const express = require("express");
+const {createSession} = require("better-sse");
+
 var traceProcessor = require("./tools/traceProcessor");
 
 const cors = require("cors");
 const app = express();
 const { PORT = 3000 } = process.env;
 
+const traceProcessorInstance = new traceProcessor();
+let cachedTxHash = undefined;
+
 app.use(cors());
 app.set("view engine", "ejs");
 app.set("views", __dirname + "/views");
 app.use(express.static("./public"));
 
+app.get("/sse", async (req, res) => {
+	let messageToSet = traceProcessorInstance.currentProcessMessage;
+	traceProcessorInstance.attachCallback((e) => session === undefined ? messageToSet = e: session.push(e, "progressChanged"));
+	const session = await createSession(req, res);
+	res.sse = session;
+	if(messageToSet != undefined){
+		res.sse.push(messageToSet);
+		messageToSet = undefined;
+	}
+	res.sse.push(cachedTxHash ? cachedTxHash : "", "txHash");
+});
 app.get("/", (req, res) => {
 	res.render("index");
 });
@@ -20,7 +36,7 @@ app.get("/tx/:txhash", (req, res) => {
 
 app.get("/transfers/:txhash", (req, res) => {
 	let txhash = req.params.txhash;
-	var traceProcessorInstance = new traceProcessor();
+	cachedTxHash = txhash;
 	traceProcessorInstance
 		.getTransfers(txhash)
 		.then(transfers => {

3b761425842841ea1a213f35efe278fa0937389e
Oliver R <whirlz2300@hotmail.com>
2022-01-10 20:29:20 +0100

    Updated front end to better reflect status. Styling update package update

--- public/js/graphDrawer.js
+++ public/js/graphDrawer.js
@@ -3,17 +3,6 @@ function drawGraph(data) {
 		rankdir: "LR"
 	});
 
-    data.nodes.forEach((node, index) => {
-        let link = "https://etherscan.io/address/" + node.address;
-        let name = node.name;
-        let nodeObj = {
-            labelType: "html",
-            label: "<a href=" + link + ">" + name + "</a>",
-            id: node.address
-        };
-        g.setNode(node.address, nodeObj);
-    });
-
     data.transfers.forEach((element, index) => {
 
         let edgeLink = "https://etherscan.io/address/" + element.token;
@@ -35,12 +24,37 @@ function drawGraph(data) {
                 // + "\n " + element.type + " \n islog " + element.isLog ,
             // curve: d3.curveBasis
         }
+        if(element.error != undefined){
+           toNodeIndex = data.nodes.findIndex(x => x.address === element.to);
+           fromNodeIndex = data.nodes.findIndex(x => x.address === element.from);
+           data.nodes[toNodeIndex].hasError = true;
+           data.nodes[fromNodeIndex].hasError = true;
+            
+        }
         g.setEdge(
             element.from,
             element.to,
             edgeObj,
             index
         );
+    });
+
+    data.nodes.forEach((node, index) => {
+        let link = "https://etherscan.io/address/" + node.address;
+        let name = node.name;
+        let nodeLabel = "<a href=" + link + ">" + name + "</a>";
+        let nodeStyle = "fill: #fff"
+        if(node.hasError){
+            nodeLabel = "&#9888; <a href=" + link + ">" + name + "</a>"
+            nodeStyle =  "fill: #fa9d9d";
+        }
+        let nodeObj = {
+            labelType: "html",
+            label: nodeLabel,
+            style: nodeStyle,
+            id: node.address
+        };
+        g.setNode(node.address, nodeObj);
     });
 
 	g.nodes().forEach(function (v) {
@@ -61,13 +75,27 @@ function drawGraph(data) {
 	svg.call(zoom);
 
     // Simple function to style the tooltip for the given node.
-    var styleTooltip = function(info, description) {
-        return "<p class='info'>" + info + "</p><p class='description'>" + description + "</p>";
+    var styleTooltip = function(to, from, type, isLog) {
+       return `<div class='transactionPopup'>
+            <div class='tofrom'>
+                <p>To : ${to}</p>
+                <p>From : ${from}</p>
+            </div>
+            <p class='transactionType'>Transaction type : ${type}</p>
+            <p class='transactionIsLog'>Is log : ${isLog}</p>
+        </div> `
     };
 
     // Simple function to style the tooltip for the given node.
-    var errorStyleTooltip = function(error) {
-        return "<p class='error'>" + error + "</p>";
+    var errorStyleTooltip = function(error, to, from, type) {
+            return `<div class='transactionPopup error'>
+            <p class="error">Error : ${error}</p>
+            <div class='tofrom'>
+                <p>To : ${to}</p>
+                <p>From : ${from}</p>
+            </div>
+            <p class='transactionType'>Transaction type : ${type}</p>
+            </div> `
     };
     
 	// Run the renderer. This is what draws the final graph.
@@ -80,15 +108,15 @@ function drawGraph(data) {
                 let transactionInfo = foundEdge.transactionInfo;
                 if(transactionInfo.error != undefined){
 
-                    let elem = $("#"+transactionInfo.to).select("rect");
-                    elem.attr("class", "node error");
-                    if(index === 0){
-                        let otherElem = $("#"+transactionInfo.from).select("rect");
-                        otherElem.attr("class", "node error");
-                    }
-                    return errorStyleTooltip("Error: " + transactionInfo.error);
+                    // let elem = $("#"+transactionInfo.to).select("rect");
+                    // elem.attr("class", "node error");
+                    // if(index === 0){
+                    //     let otherElem = $("#"+transactionInfo.from).select("rect");
+                    //     otherElem.attr("class", "node error");
+                    // }
+                    return errorStyleTooltip("Error: " + transactionInfo.error, transactionInfo.to, transactionInfo.from, transactionInfo.type);
                 }else{
-                    return styleTooltip("Useful header", "Useful description");
+                    return styleTooltip(transactionInfo.to, transactionInfo.from, transactionInfo.type, transactionInfo.isLog != undefined);
                 }
             }
         ).each(
@@ -97,9 +125,9 @@ function drawGraph(data) {
                 let foundEdge = g.edge(v);
                 let transactionInfo = foundEdge.transactionInfo;
                 let tipsyObj = { gravity: "s", opacity: 0.9, html: true };
+                tipsyObj.hoverlock = true;
                 if(transactionInfo.error != undefined)
                 {
-                    tipsyObj.hoverlock = true;
                     tipsyObj.className = "error";
                 }
                 $(this).tipsy(tipsyObj); 
@@ -134,6 +162,17 @@ async function getTransfers() {
 	return data;
 }
 
-getTransfers().then(txObj => {{
-    drawGraph(txObj);
-  }});
+function onStartGetTransfers(){
+        
+	let pathname = window.location.pathname;
+	let _txhash = pathname.substring(4);
+
+	if (_txhash.length != 66) {
+        return;
+	}
+    getTransfers().then(txObj => {{
+        drawGraph(txObj);
+    }});
+}
+
+onStartGetTransfers();
--- tools/traceProcessor.js
+++ tools/traceProcessor.js
@@ -19,6 +19,7 @@ class traceProcessor {
       let web3Instance = new Web3(new Web3.providers.HttpProvider(config.httpGethProvider));
       this.web3 = this.extendWeb3(web3Instance);
       this.txs = [];
+
     }
     
    extendWeb3(_web3Instance) {
@@ -39,19 +40,36 @@ class traceProcessor {
 
     async doGetTransfers(txHash){
         
+        this.fireProcessChangedEvent("Getting trace");
         let rawTransferData = await getTrace(txHash, this.web3);
         let callObject = rawTransferData.callObject;
 
+        this.fireProcessChangedEvent("Start processing calls");
         let processedCalls = processCalls(callObject, abiDecoder);
 
         let receipt = rawTransferData.receipt;
+        this.fireProcessChangedEvent("Start processing logs");
 		let decodedLogs = abiDecoder.decodeLogs(receipt.logs);
         let processedLogs = processLogs(decodedLogs);
 
+        this.fireProcessChangedEvent("Combining logs and calls");
         let combinedTxsAndLogs = combineTxsAndLogs(processedLogs, processedCalls);
 
+        this.fireProcessChangedEvent("Translating logs and calls");
         let nodesAndTxs = await translateCallsAndLogs(combinedTxsAndLogs, this.web3, receipt.from, erc20abi);
+        this.fireProcessChangedEvent("Done !");
         return nodesAndTxs;
+    }
+
+    fireProcessChangedEvent(processMessage){
+        this.currentProcessMessage = processMessage;
+        if(this.callbackFn !== undefined){
+            this.callbackFn(processMessage);
+        }
+    }
+
+    attachCallback(callbackFn){
+        this.callbackFn = callbackFn;
     }
 }
 

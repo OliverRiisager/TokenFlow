70c98e43f71f8eafae9d7470be425bf1841ea4d9
SpaghettiTheYeti <oliverriisager@gmail.com>
2022-03-06 20:06:07 +0100

    no message

--- config.ts
+++
@@ -1,8 +0,0 @@
-class config {
-    httpGethProvider: string;
-    constructor(){
-        this.httpGethProvider = 'https://misty-falling-breeze.quiknode.pro/1c2756414de5eb68ef4be863f6b7c2671d814a19/';
-    }
-}
-
-export {config};

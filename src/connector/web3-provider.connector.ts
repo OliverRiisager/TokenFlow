69db1238439e9b48b13fa4ffb13bb6205cf71d4d
OliverRiisager <oliver@mrswordsmith.com>
2022-03-25 22:02:37 +0100

    res rej - split back into contract names by itself together with tokensymbols and decimals. Seems to fail in some cases.

--- src/connector/web3-provider.connector.ts
+++ src/connector/web3-provider.connector.ts
@@ -3,16 +3,16 @@ import { BatchRequest } from "web3-core";
 import { ProviderConnector } from "./provider.connector";
 import { CallObject, Receipt } from "../model";
 import { ConvertReceipt, ConvertCallObject } from "../jsonConverters";
-import { SymbolDecimal } from "../knownAddresses";
+import { tokenAddressToSymbolDecimals, contractAddressToNames, SymbolDecimal } from "../knownAddresses";
 import { Contract } from "web3-eth-contract";
 import erc20Abi from '../../public/abis/erc20.json';
-
 export class Web3Provider implements ProviderConnector{
 
     protected readonly web3: Web3;
 
     protected batch: BatchRequest;
     
+    protected addressToTokens: Map<string, Contract> = new Map<string, Contract>();
     protected addressToContractMap: Map<string, Contract> = new Map<string, Contract>();
 
     constructor(web3: Web3){
@@ -58,84 +58,95 @@ export class Web3Provider implements ProviderConnector{
     {
         const resolvedTokenSymbolsAndDecimals: { address: string; symbolDecimal: SymbolDecimal; }[] = [];
         const resolvedContractNames: {address:string, name:string}[] = [];
-
         contractAddresses.forEach(element => {
-            if(!this.addressToContractMap.has(element)){
-                this.addressToContractMap.set(element, new this.web3.eth.Contract(erc20Abi as any, element));
+            if(!contractAddressToNames.hasContractAddress(element)){
+                if(!this.addressToContractMap.has(element)){
+                    this.addressToContractMap.set(element, new this.web3.eth.Contract(erc20Abi as any, element));
+                }
             }
         });
         tokenAddresses.forEach(element => {
-            if(!this.addressToContractMap.has(element)){
-                this.addressToContractMap.set(element, new this.web3.eth.Contract(erc20Abi as any, element));
+            if(!tokenAddressToSymbolDecimals.hasTokenAddress(element)){
+                if(!this.addressToContractMap.has(element)){
+                    this.addressToContractMap.set(element, new this.web3.eth.Contract(erc20Abi as any, element));
+                }
             }
         });
-        const tokenSymbolPromises: Promise<string>[] = [];
-        const tokenDecimalsPromises: Promise<number>[] = [];
-        const contractPromises: Promise<string>[] = [];
-        this.addressToContractMap.forEach((contract, address) => {
-            const symbolCall : () => string = contract.methods.symbol().call;
-            const decimalsCall: () => number = contract.methods.decimals().call;
-            const nameCall: () => string = contract.methods.name().call;
-            this.pushPromise(tokenSymbolPromises, symbolCall, address);
-            this.pushPromise(tokenDecimalsPromises, decimalsCall, 18);
-            this.pushPromise(contractPromises, nameCall, address);
-        });
-        this.batch.execute();
+        const promst: Promise<[string, number, string, string]>[] = [];
+        // const promises: Promise<{symbol:string, decimals:number, name:string, address:string}>[] = [];
+        // const tokenSymbolPromises: Promise<{symbol:string|number, backup:string|number}>[] = [];
+        // const tokenDecimalsPromises: Promise<{decimals:string|number, backup:string|number}>[] = [];
+        // const contractPromises: Promise<{contractName: string|number, backup:string|number}>[] = [];
         
-        const tokenSymbolInformation = await this.resolvePromises(tokenSymbolPromises);
-        const tokenDecimalsInformation = await this.resolvePromises(tokenDecimalsPromises);
-        for (let index = 0; index < tokenSymbolInformation.length; index++) {
-            resolvedTokenSymbolsAndDecimals.push({
-                address: tokenSymbolInformation[index].address,
-                symbolDecimal: new SymbolDecimal(
-                    tokenSymbolInformation[index].symbol,
-                    tokenDecimalsInformation[index]
-                )
-            })
-        }
-
-        const contractInformation = await this.resolvePromises(contractPromises);
-        for (let i = 0; i < contractInformation.length; i++) {
-            resolvedContractNames.push({
-                address: contractInformation[i].address,
-                name:contractInformation[i].name
-            }); 
-        }
-        return {contractNames: resolvedContractNames, tokenSymbolsAndDecimals:resolvedTokenSymbolsAndDecimals}
-    }
-
-    private pushPromise<T>(
-        promises: Promise<T>[],
-        //@ts-ignore
-        functionCall: any,
-        backup:any
-    ): void {
-        promises.push(
-            new Promise((res, rej) => {
-                const req = functionCall.request({},'latest',
-                    (err: any, data: T) => {
+        this.addressToContractMap.forEach((contract, address) => {
+            const proms = Promise.all([
+                new Promise<string>((res, rej) => {
+                    
+                    const req = contract.methods.symbol().call.request({},'latest',
+                    (err: any, data: string) => {
                         if (err) {
-                            rej({err, backup});
+                            rej({err, address});
                         } else {
                             res(data);
                         }
-                    }
-                );
-                this.batch.add(req);
-            })
-        );
-    }
-    
-    private async resolvePromises<T>(promises: Promise<T>[]): Promise<T[]> {
-        return Promise.all(
-            promises.map((p) => {
-                return p.catch((e: any) => {
-                    return e.backup ? e.backup : 'unknown';
-                });
-            })
-        );
-    }
+                    });
+                    this.batch.add(req);
+                }).catch(() => {
+                    console.log("symbol fail " + address);
+                    return address;
+                }),
 
+                new Promise<number>((res, rej) => {
+                    
+                    const req = contract.methods.decimals().call.request({},'latest',
+                    (err: any, data: number) => {
+                        if (err) {
+                            rej({err:err, val:18});
+                        } else {
+                            res(data);
+                        }
+                    });
+                    this.batch.add(req);
+                }).catch(() => {
+                    console.log("decimals fail " + address);
+                    return 18;
+                }),
+                new Promise<string>((res, rej) => {
+                    
+                    const req = contract.methods.name().call.request({},'latest',
+                    (err: any, data: string) => {
+                        if (err) {
+                            rej({err, address});
+                        } else {
+                            res(data);
+                        }
+                    });
+                    this.batch.add(req);
+                }).catch(() => {
+                    console.log("symbol fail " + address);
+                    return address;
+                }),
+                new Promise<string>(() => {
+                    return address;
+                })
+            ]);
+            promst.push(proms);
+        });
+        try{
+            console.log("PRMOSES");
+            this.batch.execute();
+            const promiseInformation = await Promise.all(promst);
+            console.log("PROM END " + promiseInformation);
+            // promiseInformation.forEach(result => {
+            //     resolvedContractNames.push({address:result.address, name:result.name});
+            //     resolvedTokenSymbolsAndDecimals.push({address:result.address, symbolDecimal:new SymbolDecimal(result.symbol, result.decimals)});
+            // })
+            return {contractNames: resolvedContractNames, tokenSymbolsAndDecimals:resolvedTokenSymbolsAndDecimals}
+        }catch(e){
+            console.log(e);
+            throw e;
+        }
+    }
     private extendWeb3(_web3Instance: Web3): Web3 {
         _web3Instance.extend({
             property: 'debug',
